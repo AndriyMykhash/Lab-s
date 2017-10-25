741ef316a03cf357af5e326f92022fde0debba79
Andriy Mykhash <32357131+AndriyMykhash@users.noreply.github.com>
2017-10-25 12:58:28 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,27 +4,35 @@ import java.util.Scanner;
 public class Fibonacci {
  
     public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
 		
-		int maxValue = 0;
-		
+		int maxValue, limitation, a = 0;
 		int numF = 0, firstF = 0, secondF = 1;
 		
 		System.out.print("Enter your value: ");
+		maxValue = sc.nextInt();
 		
-		Scanner inData = new Scanner(System.in);
-		
-		maxValue = inData.nextInt();
-		
+		System.out.print("Enter your limitation: ");
+		limitation = sc.nextInt();
 		
 		for(int i = 0; i < maxValue; i++){
+			int tempF;
 			
+			tempF = numF;
 			numF = firstF + secondF;  
-			firstF = secondF;	
+			firstF = secondF;
 			secondF = numF;
+			
+			if(numF >= limitation)
+			{	
+				a = i;
+				numF = tempF;
+				break;
+			}
 		
 		}
 		
-		System.out.print("Number Fibonacci: " + numF);
+		System.out.print(a + " number Fibonacci: " + numF);
         
     }   
 }
