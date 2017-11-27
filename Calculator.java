e57e00258295485978e0c3c4cbc0028ebca16f77
Andriy Mykhash <32357131+AndriyMykhash@users.noreply.github.com>
2017-11-27 15:29:18 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,73 +1,345 @@
 import java.util.Scanner;
 
- 
-public class Calculator {
- 
-    public static void main(String[] args) {
-       
-	   int rep;
-	   do{
-        Scanner inData = new Scanner(System.in);
-		
-		int firstNum, secondNum, resolt;
-		String chsOperation;
-		
-		
-		System.out.print("Enter your first number: ");
-		firstNum = inData.nextInt();
-		
-		
-		System.out.print("\nEnter your second number: ");
-		secondNum = inData.nextInt();
-		
-		
-		System.out.print("\nOperation \n+ \n- \n* \n/ \nChoose: ");
-		chsOperation = inData.next();
-		
-		
-		switch (chsOperation){
-			
-			case "+":
-				resolt = firstNum + secondNum;
-				System.out.println("\nYor resolt: " + resolt);
-				break;
-				
-			case "*":
-				resolt = firstNum * secondNum;
-				System.out.println("\nYor resolt: " + resolt);
-			
-				break;
-				
-			case "/":
-			
-				if(secondNum != 0){
-				resolt = firstNum / secondNum;
-				System.out.println("\nYor resolt: " + resolt);
-				}
-				
-				else{
-					
-				System.out.println("\nUnknown operation");}
-		
-			
-				break;
-			
-			case "-":
-			
-				resolt = firstNum - secondNum;
-				System.out.println("\nYor resolt: " + resolt);
-				break;
-				
-			default: System.out.println("\nUnknown operation");
-		}
-		
-		System.out.print("\nRepeat \n1 - yes\n2 - no \n");
-		rep	= inData.nextInt();
-        
-	   }while(rep == 1);
-		
-		
-		
-    }   
-	
+
+public class Main {
+
+    public static void main(String[] args){
+    
+
+        Scanner sc = new Scanner(System.in);
+        Calculator calk = new Calculator();
+        String operation, operTwo = "";
+
+        while (true) {
+
+            System.out.print("Enter operation \n  \'+\' \n  \'-\' \n  \'*\' \n  \'/\' \n  \'M\' \nif you want to exit, print - \'exit\'\n >: ");
+            operation = sc.next();
+            if (!operation.equalsIgnoreCase("M") &&
+                    !operation.equalsIgnoreCase("exit") &&
+                    !operation.equalsIgnoreCase("+") &&
+                    !operation.equalsIgnoreCase("-") &&
+                    !operation.equalsIgnoreCase("*") &&
+                    !operation.equalsIgnoreCase("/")
+                    )
+            {
+                System.out.println("      Error:\n  \'"+operation+"\' isn't operation\n");
+                continue;
+            }
+
+            if (operation.equalsIgnoreCase("M")) {
+                System.out.print("Enter next operation: ");
+                operTwo = sc.next();
+
+                if (!operTwo.equalsIgnoreCase("+") &&
+                        !operTwo.equalsIgnoreCase("-") &&
+                        !operTwo.equalsIgnoreCase("*") &&
+                        !operTwo.equalsIgnoreCase("/")
+                        )
+                {
+                    System.out.println("      Error:\n  \'"+ operTwo+"\' is not operation\n");
+                    continue;
+                }
+            }
+            else if(operation.equalsIgnoreCase("exit"))
+            {
+                break;
+            }
+
+            System.out.print("Enter first number (\'M\' - "+calk.res+" ) \n >: ");
+            String firstNum = sc.next();
+
+            if (!firstNum.equalsIgnoreCase("M") && !calk.checkString(firstNum))
+            {
+                System.out.println("      Error \n\'"+ firstNum+"\' is not number or \'M\'\n");
+                continue;
+            }
+
+            System.out.print("Enter second number (\'M\' - "+calk.res+" ) \n >: ");
+            String secNum = sc.next();
+           if (!secNum.equalsIgnoreCase("M") && !calk.checkString(secNum)){
+                System.out.println("      Error \n\'"+ secNum+"\' is not number or \'M\'\n");
+                continue;
+            }
+
+            if (operation.equalsIgnoreCase("M")){
+                System.out.println("Result: " + calk.operWithM(operTwo, firstNum, secNum));
+            }
+            else if (!operation.equalsIgnoreCase("M")){
+                System.out.println("Result: " + calk.operNotM(operation, firstNum, secNum));
+            }
+            System.out.println();
+            }
+
+    }
+    }
+
+
+
+
+class Calculator{
+    double res = 0;
+
+    double operNotM(String operation, String firstNum, String secNum){
+            switch (operation) {
+                case "+": return plusNotM(firstNum,secNum);
+                case "-": return minusNotM(firstNum,secNum);
+                case "*": return multiplyNotM(firstNum,secNum);
+                case "/": return divideNotM(firstNum,secNum);
+
+        }
+        return 0;
+    }
+    double operWithM(String operTwo, String firstNum, String secNum){
+        switch (operTwo) {
+            case "+": return plusM(firstNum,secNum);
+            case "-": return minusM(firstNum,secNum);
+            case "*": return multiplyM(firstNum,secNum);
+            case "/": return divideM(firstNum,secNum);
+
+        }
+        return 0;
+    }
+
+    private double plusNotM(String firstNum, String secNum){
+        double first = 0;
+        double second = 0;
+        if (firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = Double.parseDouble(secNum);
+        }
+        else if (!firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = res;
+
+        }
+
+        else if(firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = res;
+        }
+        else {
+            first = Double.parseDouble(firstNum);
+            second = Double.parseDouble(secNum);
+        }
+        return first + second;
+    }
+    private double minusNotM(String firstNum, String secNum){
+
+        double first = 0;
+        double second = 0;
+        if (firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = Double.parseDouble(secNum);
+        }
+        else if (!firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = res;
+
+        }
+        else if(!firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = Double.parseDouble(secNum);
+        }
+        else if(firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = res;
+        }
+        return first - second;
+    }
+    private double multiplyNotM(String firstNum, String secNum){
+        double first = 0;
+        double second = 0;
+        if (firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = Double.parseDouble(secNum);
+        }
+        else if (!firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = res;
+
+        }
+        else if(!firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = Double.parseDouble(secNum);
+        }
+        else if(firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = res;
+        }
+        return first * second;
+    }
+    private double divideNotM(String firstNum, String secNum){
+        double first = 0;
+        double second = 0;
+        if (firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = Double.parseDouble(secNum);
+        }
+        else if (!firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = res;
+
+        }
+        else if(!firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = Double.parseDouble(secNum);
+        }
+        else if(firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = res;
+        }
+
+        return first / second;
+    }
+
+    private double plusM(String firstNum, String secNum){
+        double first = 0;
+        double second = 0;
+
+        if (firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = Double.parseDouble(secNum);
+        }
+        else if (!firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = res;
+
+        }
+        else if(!firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = Double.parseDouble(secNum);
+        }
+        else if(firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = res;
+        }
+        res = first + second;
+        return res;
+    }
+    private double minusM(String firstNum, String secNum){
+        double first = 0;
+        double second = 0;
+
+        if (firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = Double.parseDouble(secNum);
+        }
+        else if (!firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = res;
+
+        }
+        else if(!firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = Double.parseDouble(secNum);
+        }
+        else if(firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = res;
+        }
+        res = first - second;
+        return res;
+
+    }
+    private double multiplyM(String firstNum, String secNum){
+        double first = 0;
+        double second = 0;
+
+        if (firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = Double.parseDouble(secNum);
+        }
+        else if (!firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = res;
+
+        }
+        else if(!firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = Double.parseDouble(secNum);
+        }
+        else if(firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = res;
+        }
+        res = first * second;
+        return res;
+
+    }
+    private double divideM(String firstNum, String secNum){
+        double first = 0;
+        double second = 0;
+
+        if (firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = Double.parseDouble(secNum);
+        }
+        else if (!firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = res;
+
+        }
+        else if(!firstNum.equalsIgnoreCase("M") &&
+                !secNum.equalsIgnoreCase("M")){
+            first = Double.parseDouble(firstNum);
+            second = Double.parseDouble(secNum);
+        }
+        else if(firstNum.equalsIgnoreCase("M") &&
+                secNum.equalsIgnoreCase("M")){
+            first = res;
+            second = res;
+        }
+        res = first / second;
+        return res;
+    }
+
+    public boolean checkString(String string) {
+        if (string == null || string.length() == 0) return false;
+
+        int i = 0;
+        if (string.charAt(0) == '-') {
+            if (string.length() == 1) {
+                return false;
+            }
+            i = 1;
+        }
+
+        char c;
+        for (; i < string.length(); i++) {
+            c = string.charAt(i);
+            if (!(c >= '0' && c <= '9')) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
